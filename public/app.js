b41abd90c22135ae4ab52d16995ed0b967a2d58e
trig010 <trig.code@gmail.com>
2017-04-18 21:49:03 -0500

    added sytling and custom errors

--- public/app.js
+++ public/app.js
@@ -1,43 +1,50 @@
 //signup user
 $('#js-signup-form').submit(function(event) {
     event.preventDefault();
-    let obj = {
-        username: event.target.username.value,
-        password: event.target.password.value
+    if((!event.target.username.value || !event.target.password.value)){
+        $('.alert.alert-warning').show(200).append('Please enter a username and a password')
+    } else {
+        let obj = {
+            username: event.target.username.value,
+            password: event.target.password.value
+        }
+
+        //clear form fields
+        $('input, textarea').val('');
+
+        $.ajax({type: 'POST', data: JSON.stringify(obj), url: '/signup', contentType: "application/json", dataType: "json"}).then(function(res) {
+            // console.log('RES: ', res)
+            window.location = '/stocksaver.html';
+        }).fail(function(err) {
+            console.log('SIGNUP FAIL')
+            $('.alert.alert-warning').text("")
+            console.log(err)
+        })
     }
-
-    //clear form fields
-    $('input, textarea').val('');
-
-    $.ajax({type: 'POST', data: JSON.stringify(obj), url: '/signup', contentType: "application/json", dataType: "json"}).then(function(res) {
-        // console.log('RES: ', res)
-        window.location = '/stocksaver.html';
-    }).fail(function(err) {
-        console.log('SIGNUP FAIL')
-        $('.alert.alert-warning').text("")
-        console.log(err)
-    })
 });
 
 //login user
 $('#js-login-form').on('submit', function(event) {
     event.preventDefault();
-    user = {
-        username: event.target.username.value,
-        password: event.target.password.value
+    if((!event.target.username.value || !event.target.password.value)){
+        $('.alert.alert-warning').show(200).append('Please enter a username and a password')
+    } else {
+        user = {
+            username: event.target.username.value,
+            password: event.target.password.value
+        }
+        //clear form fields
+        $('.form-group').val('');
+
+        $.ajax({type: 'POST', data: JSON.stringify(user), url: '/login', contentType: "application/json", dataType: "json"}).then(function() {
+            window.location = '/stocksaver.html';
+        }).fail(function(err) {
+            console.log('LOGIN FAIL')
+            $('.alert.alert-warning').text("")
+            $('.alert.alert-warning').show(200).append("Please try again.")
+            console.log(err);
+        })
     }
-
-    //clear form fields
-    $('.form-group').val('');
-
-    $.ajax({type: 'POST', data: JSON.stringify(user), url: '/login', contentType: "application/json", dataType: "json"}).then(function() {
-        window.location = '/stocksaver.html';
-    }).fail(function(err) {
-        console.log('LOGIN FAIL')
-        $('.alert.alert-warning').text("")
-        $('.alert.alert-warning').show(200).append("Please try again.")
-        console.log(err);
-    })
 });
 
 //get username
@@ -86,40 +93,54 @@ $('#delete-user').on('click', function() {
 
 let obj;
 
+function onlyLetters(input)
+  {
+   let letters = /^[A-Za-z]+$/;
+   if(input.match(letters))
+     { return true; }
+   else { return false; }
+  }
+
 //stock search
 $('#stock-search').submit(function(event) {
     event.preventDefault();
-    let stock = event.target.stock.value;
-    $('#results').hide(200).val('')
-    $('.alert').hide(200).html('')
-    $('#save-button').hide(200)
-    $('input, textarea').val('');
+    $('.alert.alert-warning').text("");
+    if((!event.target.stock.value) || onlyLetters(event.target.stock.value) !== true){
+        $('.alert.alert-warning').show(200).append('Please enter a valid stock symbol')
+    } else {
+        console.log('TYPE: ', typeof event.target.stock.value);
+        let stock = event.target.stock.value;
+        $('#results').hide(200).val('')
+        $('.alert').hide(200).html('')
+        $('#save-button').hide(200)
+        $('input, textarea').val('');
 
-    $.ajax({
-        type: 'GET',
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stock + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
-    }).then(function(res) {
-        let companyName = res.query.results.quote.Name;
-        let askPrice = res.query.results.quote.Ask;
-        let lastPrice = res.query.results.quote.LastTradePriceOnly;
-        let date = new Date()
-        let options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'
-        };
-        let dateTime = new Intl.DateTimeFormat('en-US', options).format(date);
-        obj = {
-            stock: stock,
-            price: lastPrice,
-            time: dateTime
-        }
-        console.log(obj.time)
-        if (companyName === null) {
-            $('.alert').show(300).html('Sorry, that stock was not found.')
-        } else {
+        $.ajax({
+            type: 'GET',
+            url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stock + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+        }).then(function(res) {
+            let companyName = res.query.results.quote.Name;
+            let askPrice = res.query.results.quote.Ask;
+            let lastPrice = res.query.results.quote.LastTradePriceOnly;
+            let date = new Date()
+            let options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'
+            };
+            let dateTime = new Intl.DateTimeFormat('en-US', options).format(date);
+            obj = {
+                stock: stock,
+                price: lastPrice,
+                time: dateTime
+            }
+            console.log(obj.time)
+            if (companyName === null) {
+                $('.alert').show(300).html('Sorry, that stock was not found.')
+            } else {
 
-            $('#results').show(300).html('The last trading price of ' + companyName + ' (' + stock + ') was: $' + lastPrice + '.')
-            $('#save-stocks-button').show(1000)
-        }
-    });
+                $('#results').show(300).html('The last trading price of ' + companyName + ' (' + stock + ') was: $' + lastPrice + '.')
+                $('#save-stocks-button').show(1000)
+            }
+        });
+    }
 })
 
 //save stocks
@@ -127,7 +148,7 @@ $('#save-stocks-button').on('click', function(event) {
     $('#saved-stocks-empty').hide()
     $.ajax({type: 'POST', data: JSON.stringify(obj), url: '/stocksaver/stocks', contentType: "application/json", dataType: "json"}).then(function(res) {
         console.log('save res: ', res)
-            $('#stocks-table').prepend( `<tr><td>${obj.stock}</td><td>$${obj.price}</td><td>${obj.time}</td><td><a class="btn btn-small" id="delete-stock-button" value="${obj._id}"><i class="fa fa-times" aria-hidden="true"></i> Delete Stock</a></td></tr>`)
+            $('#stocks-table').prepend( `<tr><td>${obj.stock}</td><td>$${obj.price}</td><td>${obj.time}</td><td><a class="btn btn-small" id="delete-stock-button" value="${obj._id}"><i class="fa fa-times" aria-hidden="true"></i> </a></td></tr>`)
     }).fail(function(err) {
         console.log('AJAX FAIL')
         $('.alert.alert-warning').toggle(300).html('ERROR')
@@ -147,7 +168,7 @@ $('#view-stocks-button').on('click', function() {
         } else {
             $('#saved-stocks-empty').hide()
             let stocksList = Object.keys(stocks).map(function(key) {
-                return `<tr><td>${stocks[key].stock}</td><td>$${stocks[key].price}</td><td>${stocks[key].time}</td><td><a class="btn btn-small" id="delete-stock-button" value="${stocks[key]._id}"><i class="fa fa-times" aria-hidden="true"></i> Delete Stock</a></td></tr>`
+                return `<tr><td>${stocks[key].stock}</td><td>$${stocks[key].price}</td><td>${stocks[key].time}</td><td><a class="btn btn-small" id="delete-stock-button" value="${stocks[key]._id}"><i class="fa fa-times" aria-hidden="true"></i> </a></td></tr>`
             }).join("");
             $('#saved-stocks').show(200)
             $('#stocks-table').html(`${stocksList}`)
